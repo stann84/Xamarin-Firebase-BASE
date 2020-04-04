ca618807522d7fc560eb685ee8f0afaddbd5fb10
stan bomin <stann84@hotmail.com>
2020-04-04 16:50:40 +0200

    Ajoutez des fichiers projet.

---
+++ Xamarin-Firebase-master/TestPhoto/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestXamarinFirebase.Model
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string PhotoUrl { get; set; }
+        public string PhotoName { get; set; }
+        public string Tel { get; set; }
+        public string Prenom { get; set; }
+        public string Nom { get; set; }
+    }
+}
